8eab2e441e6d1276d1132981527d269633076592
Lal <lal.zada@ssidecisions.com>
2019-09-02 17:19:33 +0500

    copy replaced with deepcopy

--- rest_toolbox/optional_fields.py
+++ rest_toolbox/optional_fields.py
@@ -33,7 +33,7 @@ class OptionalFieldsMixin(object):
         assert has_owner_attr or not has_owner_fields, OWNER_FIELDS_NO_ATTR
 
     def get_fields(self):
-        fields = super(OptionalFieldsMixin, self).get_fields().copy()
+        fields = super(OptionalFieldsMixin, self).get_fields().deepcopy()
 
         for name, field in fields.items():
             if self.allow_field(name, field) and self.show_field(name, field):
